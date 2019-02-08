a3dd71ca02487ce72930a949cea7937dcd214335
pritcool2003 <pritam.tamsekar@gmail.com>
2019-02-08 17:14:22 +0530

    twitter credential code added

---
+++ credentials.py
@@ -0,0 +1,6 @@
+# Twitter keys and access tokens need to be specified once you create your twitter application.
+# The information in this file is meant for private use so be careful not to share your keys
+consumer_key = 'Enter consumer_key obtained from twitter'
+consumer_secret = 'Enter consumer_secret obtained from twitter'
+access_token = 'Enter access_token obtained from twitter'
+access_token_secret = 'Enter access_token_secret obtained from twitter'
